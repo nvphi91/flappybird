5cff0ea754ce72248edf60c8cd79700b8690f4a3
Nguyen Van Phi <nvphi91@gmail.com>
2015-04-14 17:46:07 +0700

    - update game

--- FlappyBird/GameScene.m
+++ FlappyBird/GameScene.m
@@ -8,20 +8,38 @@
 
 #import "GameScene.h"
 
-@interface GameScene(){
+@interface GameScene()<SKPhysicsContactDelegate>{
     SKSpriteNode *birdSprite;
     SKColor *skyColor;
     SKTexture *pipe1Texture;
     SKTexture *pipe2Texture;
     SKAction *moveandremovePipes;
+    SKNode *moving;
+    SKNode *pipes;
+    BOOL canRestart;
+    
+    SKLabelNode *scoreLable;
+    NSInteger score;
 }
 @end
 @implementation GameScene
 
 static NSInteger const KVerticalPipeGap = 100;
+static const uint32_t birdCategory = 1 << 0;
+static const uint32_t worldCategory = 1 << 1;
+static const uint32_t pipeCategory = 1 << 2;
 
 -(id) initWithSize:(CGSize)size{
     if (self == [super initWithSize:size]) {
+        canRestart = NO;
+        score = 0;
+        
+        scoreLable = [SKLabelNode labelNodeWithFontNamed:@"MarkerFelt-Wide"];
+        scoreLable.position = CGPointMake(CGRectGetMidX(self.frame), 3*self.frame.size.height/4);
+        scoreLable.zPosition = 100;
+        scoreLable.text = [NSString stringWithFormat:@"%ld",(long)score];
+        [self addChild:scoreLable];
+        
         SKTexture *bird1Texture = [SKTexture textureWithImageNamed:@"Bird1"];
         bird1Texture.filteringMode = SKTextureFilteringNearest;
         
@@ -37,10 +55,19 @@ static NSInteger const KVerticalPipeGap = 100;
         birdSprite.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:birdSprite.size.height/2];
         birdSprite.physicsBody.dynamic = YES;
         birdSprite.physicsBody.allowsRotation = NO;
+        birdSprite.physicsBody.categoryBitMask = birdCategory;
+        birdSprite.physicsBody.collisionBitMask = worldCategory|pipeCategory;
+        birdSprite.physicsBody.contactTestBitMask = worldCategory|pipeCategory;
         [self addChild:birdSprite];
         
         skyColor = [SKColor colorWithRed:113.0/255.0 green:197.0/255.0 blue:207.0/255.0 alpha:1.0];
         [self setBackgroundColor: skyColor];
+        
+        moving = [SKNode node];
+        [self addChild:moving];
+        
+        pipes = [SKNode node];
+        [moving addChild:pipes];
         
         SKTexture *groundTexture = [SKTexture textureWithImageNamed:@"Ground"];
         groundTexture.filteringMode = SKTextureFilteringNearest;
@@ -54,7 +81,9 @@ static NSInteger const KVerticalPipeGap = 100;
             [groundSprite setScale:2.0];
             groundSprite.position = CGPointMake(i*groundSprite.size.width, groundSprite.size.height/2);
             [groundSprite runAction:moveGroundSpriteForever];
-            [self addChild:groundSprite];
+//            [self addChild:groundSprite];
+            
+            [moving addChild:groundSprite];
         }
         
         SKTexture *skyLineTexture = [SKTexture textureWithImageNamed:@"Skyline"];
@@ -70,16 +99,21 @@ static NSInteger const KVerticalPipeGap = 100;
             skyLineSpite.zPosition = -20;
             skyLineSpite.position = CGPointMake(i * skyLineSpite.size.width, skyLineSpite.size.height + groundTexture.size.height);
             [skyLineSpite runAction:moveSkyLineSpriteForever];
-            [self addChild:skyLineSpite];
+//            [self addChild:skyLineSpite];
+            
+            [moving addChild:skyLineSpite];
         }
         
         SKNode *dummy = [SKNode node];
         dummy.position = CGPointMake(0, groundTexture.size.height);
         dummy.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(self.frame.size.width, groundTexture.size.height *2)];
         dummy.physicsBody.dynamic = NO;
+        dummy.physicsBody.categoryBitMask = worldCategory;
         [self addChild:dummy];
         
         self.physicsWorld.gravity = CGVectorMake(0.0, -5.0);
+        self.physicsWorld.contactDelegate = self;
+        self.view.showsPhysics = YES;
         
         // create pipes;
         pipe1Texture = [SKTexture textureWithImageNamed:@"Pipe1"];
@@ -104,7 +138,7 @@ static NSInteger const KVerticalPipeGap = 100;
 
 -(void) spawnPipes{
     SKNode *pipePair = [SKNode node];
-    pipePair.position = CGPointMake(self.frame.size.width + pipe1Texture.size.width, 0);
+    pipePair.position = CGPointMake(self.frame.size.width + pipe1Texture.size.width*2, 0);
     pipePair.zPosition = -10;
     
     CGFloat y = arc4random() % (NSInteger)(self.frame.size.height/3);
@@ -114,6 +148,8 @@ static NSInteger const KVerticalPipeGap = 100;
     pipe1Sprite.position = CGPointMake(0, y);
     pipe1Sprite.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:pipe1Sprite.size];
     pipe1Sprite.physicsBody.dynamic = NO;
+    pipe1Sprite.physicsBody.categoryBitMask = pipeCategory;
+    pipe1Sprite.physicsBody.contactTestBitMask = birdCategory;
     [pipePair addChild:pipe1Sprite];
     
     SKSpriteNode *pipe2Sprite = [SKSpriteNode spriteNodeWithTexture:pipe2Texture];
@@ -121,12 +157,22 @@ static NSInteger const KVerticalPipeGap = 100;
     pipe2Sprite.position = CGPointMake(0, y + pipe1Sprite.size.height + KVerticalPipeGap);
     pipe2Sprite.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:pipe2Sprite.size];
     pipe2Sprite.physicsBody.dynamic = NO;
+    pipe2Sprite.physicsBody.categoryBitMask = pipeCategory;
+    pipe2Sprite.physicsBody.contactTestBitMask = birdCategory;
     [pipePair addChild:pipe2Sprite];
     
 //    SKAction *movePipes = [SKAction repeatActionForever:[SKAction moveByX:-1 y:0 duration:0.02]];
+    
+    SKNode *contactNode = [SKNode node];
+    contactNode.position = CGPointMake(pipe1Sprite.size.width + birdSprite.size.width/2, CGRectGetMidY(self.frame));
+    contactNode.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(pipe2Sprite.size.width, self.frame.size.height)];
+    contactNode.physicsBody.dynamic = NO;
+    [pipePair addChild:contactNode];
+    
     [pipePair runAction:moveandremovePipes];
     
-    [self addChild:pipePair];
+//    [moving addChild:pipePair];
+    [pipes addChild:pipePair];
 }
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
@@ -135,9 +181,26 @@ static NSInteger const KVerticalPipeGap = 100;
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    
+    if (moving.speed > 0) {
+        birdSprite.physicsBody.velocity = CGVectorMake(0, 0);
+        [birdSprite.physicsBody applyImpulse:CGVectorMake(0, 4)];
+    }
+    else if(canRestart){
+        [self resetScene];
+    }
+}
+
+-(void) resetScene{
+    birdSprite.position = CGPointMake(self.frame.size.width/4, CGRectGetMidY(self.frame));
     birdSprite.physicsBody.velocity = CGVectorMake(0, 0);
-    [birdSprite.physicsBody applyImpulse:CGVectorMake(0, 4)];
+    birdSprite.physicsBody.collisionBitMask = worldCategory | pipeCategory;
+    birdSprite.speed = 1.0;
+    birdSprite.zRotation = 0.0;
+    [pipes removeAllChildren];
+    
+    canRestart = NO;
+    
+    moving.speed = 1;
 }
 
 CGFloat clamp(CGFloat min, CGFloat max, CGFloat value){
@@ -152,7 +215,45 @@ CGFloat clamp(CGFloat min, CGFloat max, CGFloat value){
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
     
-    birdSprite.zPosition = clamp(-1, 0.5, birdSprite.physicsBody.velocity.dy*(birdSprite.physicsBody.velocity.dy < 0 ? 0.03 : 0.01));
+    if (moving.speed > 0) {
+        birdSprite.zPosition = clamp(-1, 0.5, birdSprite.physicsBody.velocity.dy*(birdSprite.physicsBody.velocity.dy < 0 ? 0.03 : 0.01));
+    }
+}
+
+-(void) didBeginContact:(SKPhysicsContact *)contact{
+    
+    moving.speed = 0;
+    
+    birdSprite.physicsBody.collisionBitMask = worldCategory;
+    
+//    SKPhysicsBody *body1 = contact.bodyA;
+//    SKPhysicsBody *body2 = contact.bodyB;
+    
+//    [birdSprite runAction:[SKAction rotateByAngle:M_PI*birdSprite.position.y*0.01 duration:birdSprite.position.y*0.03] completion:^{
+//        birdSprite.speed = 0;
+//    }];
+    [self removeActionForKey:@"flash"];
+    [self runAction:[SKAction sequence:@[[SKAction repeatAction:[SKAction sequence:@[[SKAction runBlock:^{self.backgroundColor = [SKColor redColor];}],[SKAction waitForDuration:0.05], [SKAction runBlock:^{
+        self.backgroundColor = skyColor;
+    }],[SKAction waitForDuration:0.05]]] count:4],[SKAction runBlock:^{
+        canRestart = YES;
+    }]]] withKey:@"flash"];
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
